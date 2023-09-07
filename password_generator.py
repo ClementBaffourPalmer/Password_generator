74daf062d336dbf184df45b23fcbaea0f1cee507
ClementKwesiBaffourPalmer <clementbaffour@Drs-MacBook-Pro.local>
2023-09-07 11:53:52 +0000

    Initial commit

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,8 @@ import string
 
 def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = ''.join(random.choice(characters) 
+ for _ in range(length))
     return password
 
 if __name__ == "__main__":
